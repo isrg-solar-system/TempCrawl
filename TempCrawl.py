09634e4fbb785514a675a70f00d62d6641bd56c0
hao <junhao8651@gmail.com>
2018-03-09 16:23:04 +0800

    upload to mysql fail

--- TempCrawl.py
+++ TempCrawl.py
@@ -15,13 +15,8 @@ from headers import header
 
 start = time.time()
 starttime = int(time.strftime("%M", time.localtime()))
-yesterday = datetime.datetime.now().strftime("%Y%m%d")
-# try:
-    #     db = pymysql.connect(host='60.249.6.104', port=33060, user='root', passwd='ncutim', db='Listing',
-    #                          charset='utf8')
-    # except:
-    #     pass
-    # cursor = db.cursor()
+now = datetime.datetime.now().strftime("%Y%m%d")
+
 conntime = b = int(time.strftime("%M", time.localtime()))
 proxiesList = ["http://60.249.6.104:8080", "http://60.249.6.105:8080",
                    "http://60.249.6.104:8080", "http://192.168.1.3:8080", "http://192.168.2.12:8080",
@@ -32,29 +27,40 @@ if conntime - starttime >= 1:
 else:  # 找可用IP塞到list
     headers = {'user-agent': "my-app/0.0.1"}
     proxies = {'proxy': "http://192.168.2.12:8080"}
-res = requests.get('http://opendata.epa.gov.tw/ws/Data/ATM00698/?$skip=0&$top=300&format=json',
+tempapi = requests.get('http://opendata.epa.gov.tw/ws/Data/ATM00698/?$skip=0&$top=300&format=json',
                        headers=headers, proxies=proxies)
-try:
-    allData = json.loads(res.text)
-    print allData[0]
-        # select = (
-        #     '''SELECT * FROM `stockdata` WHERE `date` = "''' + _data[-1][
-        #         0] + '''" and `sid`="''' + asid + '''"''')
-        # cursor.execute(select)
-        # search = cursor.fetchone()
-        # if search is not None:
-        #     pass
-        # else:
-        #     insert = (
-        #         """INSERT  INTO `stockdata` (`date`, `sid`, `name`,`market`,`coe`, `shareTrades`, `turnover`, `open`, `high`, `low`, `closing`,`grossspread`,`tradingvolume`,`time`) VALUES (%s,%s,%s,%s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s)""")
-        #     da = (_data[-1][0], asid, aname, amarket, acoe, float(st), float(to), float(open), float(high),
-        #           float(low),
-        #           float(closing),
-        #           _data[-1][7], float(tv), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        #     cursor.execute(insert, da)
-        #     db.commit()
-        # db.close()
-except:
-    pass;
+uviapi= requests.get('http://opendata2.epa.gov.tw/UV/UV.json',
+                       headers=headers, proxies=proxies)
+# 地點(SiteName)
+# 風力(WindPower)
+# 陣風(Gust)
+# 溫度(Temperature)
+# 相對溼度(Moisture)
+# 天氣狀況(Weather)
+# 日累積雨量(Rainfall1day)
+# UVI
+tempData = json.loads(tempapi.text)
+uviData = json.loads(uviapi.text)
+print tempData[0]['SiteName']
+print uviData[2]['UVI']
+# select = (
+#     '''SELECT * FROM `stockdata` WHERE `date` = "''' + _data[-1][
+#         0] + '''" and `sid`="''' + asid + '''"''')
+# cursor.execute(select)
+# search = cursor.fetchone()
+# if search is not None:
+#     pass
+# else:
+db = pymysql.connect(host='60.249.6.104', port=33060, user='root', passwd='ncutim', db='projectiot',
+                     charset='utf8')
+cursor = db.cursor()
+insert = (
+    """INSERT  INTO `Temp` (`SiteName`, `WindPower`, `Gust`,`Temperature`,`Moisture`, `Weather`, `Rainfall1day`, `UVI`,`date`,`time`) VALUES (%s,%s,%s,%s, %s, %s, %s, %s,%s,%s)""")
+da = (tempData[0]['SiteName'], tempData[0]['WindPower'], tempData[0]['Gust'], tempData[0]['Temperature'],
+      tempData[0]['Moisture'], tempData[0]['Weather'], tempData[0]['Rainfall1day'], uviData[2]['UVI'],
+      tempData[0]['DataCreationDate'], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+cursor.execute(insert, da)
+db.commit()
+db.close()
 end = time.time()
 print end - start
