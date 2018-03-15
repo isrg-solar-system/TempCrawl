6fc31678864d2b3e01a9f05cba735b2911c5b720
Jacky <junhao8651@gmail.com>
2018-03-15 22:49:34 +0800

    Enter python TempCrawl.py taichung

--- TempCrawl.py
+++ TempCrawl.py
@@ -2,22 +2,42 @@
 import requests
 import json
 import sys
-
+import time
+tempFormat = time.strftime("%Y/%m/%d %H", time.localtime())
+tempTime = tempFormat.replace(tempFormat[0:4], str(int(tempFormat[0:4])- 1911))#抓temp時間
+uviFormat = time.strftime("%Y-%m-%d %H", time.localtime())
+proxiesList = ["http://60.249.6.104:8080", "http://60.249.6.105:8080",
+               "http://60.249.6.104:8080", "http://192.168.1.3:8080"]
+proxies = {'proxy': "http://60.249.6.105:8080"}
 headers = {'user-agent': "my-app/0.0.1"}
-proxies = {'proxy': "http://192.168.2.12:8080"}
 tempapi = requests.get('http://opendata.epa.gov.tw/ws/Data/ATM00698/?$skip=0&$top=300&format=json',
-                       headers=headers, proxies=proxies)
+                       headers=headers,proxies = proxies)
 uviapi= requests.get('http://opendata2.epa.gov.tw/UV/UV.json',
-                       headers=headers, proxies=proxies)
+                       headers=headers,proxies = proxies)
 tempData = json.loads(tempapi.text)
 uviData = json.loads(uviapi.text)
-SN_list = []
-for q in range(300):#Add locations to the array to search
-    SN_list.append(tempData[q]['SiteName'])
-# print SN_list.index(u'臺中')#Taichung ID
-serdate = sys.argv[1]#107/3/11
-sertime = sys.argv[2]#0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
-if tempData[int(SN_list.index(u'臺中'))]['DataCreationDate']== serdate+" "+sertime+":00:00":
-    print tempData[int(SN_list.index(u'臺中'))]
-else:
-    print 'Error'
+serplace = sys.argv[1]#地點
+uviSN_list = []
+tempSN_list = []
+
+
+if serplace == 'taichung':
+        serplace = u'臺中'
+
+for q in range(33):#Add locations to the array to search
+    uviSN_list.append(uviData[q]['SiteName'])
+for w in range(300):#Add locations to the array to search
+    tempSN_list.append(tempData[w]['SiteName'])
+SiteName = tempData[int(tempSN_list.index(serplace))]['SiteName']
+WindPower = tempData[int(tempSN_list.index(serplace))]['WindPower']
+Gust = tempData[int(tempSN_list.index(serplace))]['Gust']
+Temperature = tempData[int(tempSN_list.index(serplace))]['Temperature']
+Moisture = tempData[int(tempSN_list.index(serplace))]['Moisture']
+Weather = tempData[int(tempSN_list.index(serplace))]['Weather']
+Rainfall1day = tempData[int(tempSN_list.index(serplace))]['Rainfall1day']
+UVI = uviData[int(uviSN_list.index(serplace))]['UVI']
+print '{["SiteName":"'+SiteName+'","WindPower":"'+WindPower+'","Gust":"'+Gust+'","Temperature":"'+Temperature+'","Moisture":"'+Moisture+'","Weather":"'+Weather+'","Rainfall1day":"'+Rainfall1day+'","UVI":"'+UVI+'"]}'
+# if tempData[int(SN_list.index("u'"+serplace+"'"))]['DataCreationDate']== serdate+" "+sertime+":00:00":
+#     print tempData[int(SN_list.index("u'"+serplace+"'"))]
+# else:
+#     print 'Error'
